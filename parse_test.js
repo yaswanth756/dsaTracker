a96f7c130f10413cc7cc260c3503c3c162343f2a
Yaswanth <vempalliyaswanth299@gmail.com>
2026-03-10 20:28:40 +0530

    chore: remove test files

--- parse_test.js
+++
@@ -1,7 +0,0 @@
-const { Editor } = require('@tiptap/core');
-const StarterKit = require('@tiptap/starter-kit').default;
-const editor = new Editor({
-  extensions: [StarterKit],
-  parseOptions: { preserveWhitespace: 'full' }
-});
-console.log(editor.options.parseOptions);
